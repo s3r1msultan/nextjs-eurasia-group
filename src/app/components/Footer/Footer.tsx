766de80c579affc5952a28819dca549c55e8dd26
s3r1msultan <ersultanserimbetov@gmail.com>
2024-06-21 04:44:01 +0500

    added right implementation of social link images

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,42 +1,33 @@
 import React from "react";
 import Wrapper from "../Wrapper/Wrapper";
 import styles from "./Footer.module.scss";
-import Image from "next/image";
 import { inter } from "@/pages/layout";
+import Link from "next/link";
+import FacebookIcon from "@/app/icons/FacebookIcon";
+import InstagramIcon from "@/app/icons/InstagramIcon";
+import YoutubeIcon from "@/app/icons/YoutubeIcon";
 
 const Footer = () => {
-	return (
-		<footer className={inter.className}>
-			<Wrapper>
-				<div className={styles.footer_content}>
-					<div className={styles.copyright}>© Eurasia Group 2022</div>
-					<div className="social_links">
-						<Image
-							src="/assets/images/facebook.png"
-							alt="Facebook icon"
-							width={0}
-							height={0}
-							sizes="100vh"
-						/>
-						<Image
-							src="/assets/images/instagram.png"
-							alt="Instagram icon"
-							width={0}
-							height={0}
-							sizes="100vh"
-						/>
-						<Image
-							src="/assets/images/youtube.png"
-							alt="Youtube icon"
-							width={0}
-							height={0}
-							sizes="100vh"
-						/>
-					</div>
-				</div>
-			</Wrapper>
-		</footer>
-	);
+  return (
+    <footer className={`${inter.className} ${styles.footer}`}>
+      <Wrapper>
+        <div className={styles.footer_content}>
+          <div className={styles.footer_copyright}>© Eurasia Group 2022</div>
+          <div className={styles.footer_social_links}>
+            <Link className={styles.footer_social_link} href="">
+              <FacebookIcon className={styles.icon + " " + styles.facebook} />
+            </Link>
+            <Link className={styles.footer_social_link} href="">
+              <InstagramIcon className={styles.icon + " " + styles.instagram} />
+            </Link>
+            <Link className={styles.footer_social_link} href="">
+              <YoutubeIcon className={styles.icon + " " + styles.youtube} />
+            </Link>
+          </div>
+        </div>
+      </Wrapper>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/app/components/Sidebar/Sidebar.tsx
+++ src/app/components/Sidebar/Sidebar.tsx
@@ -5,96 +5,71 @@ import Link from "next/link";
 import { inter } from "@/pages/layout";
 import { ArrowTopRight } from "@/app/icons/ArrowTopRight";
 import Image from "next/image";
+import FacebookIcon from "@/app/icons/FacebookIcon";
+import InstagramIcon from "@/app/icons/InstagramIcon";
+import YoutubeIcon from "@/app/icons/YoutubeIcon";
 
 interface SidebarProps {
-	isOpen: boolean;
-	showSidebar: (isOpen: boolean) => void;
+  isOpen: boolean;
+  showSidebar: (isOpen: boolean) => void;
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ isOpen, showSidebar }) => {
-	const sidebarRef = useOnClickOutside({
-		onClickOutside: () => {
-			if (isOpen) {
-				showSidebar(false);
-			}
-		},
-	});
+  const sidebarRef = useOnClickOutside({
+    onClickOutside: () => {
+      if (isOpen) {
+        showSidebar(false);
+      }
+    },
+  });
 
-	return (
-		<>
-			<nav
-				className={`${styles.nav} ${inter.className} ${
-					isOpen ? styles.open : ""
-				}`}
-				ref={sidebarRef}>
-				<button
-					onClick={() => {
-						showSidebar(false);
-					}}
-					className={styles.close_button}>
-					<div className={styles.close_button_wrapper}>
-						<div className={styles.close_line}></div>
-						<div className={styles.close_line}></div>
-					</div>
-				</button>
-				<ul className={styles.nav_links}>
-					<li>
-						<Link href={"/"}>О компании</Link>
-					</li>
-					<li>
-						<Link href={"/our_mission"}>Наша миссия</Link>
-					</li>
-					<li>
-						<Link href={"/news"}>Что нового</Link>
-					</li>
-				</ul>
-				<button className={styles.contact_us}>
-					<span>Связаться с нами</span> <ArrowTopRight />
-				</button>
-				<div className={styles.credits}>
-					<ul className="social_links">
-						<li>
-							<a target="_blank" href="#">
-								<Image
-									width={0}
-									height={0}
-									src="/assets/images/facebook-filled.png"
-									alt="Facebook icon"
-									sizes="100vh"
-								/>
-							</a>
-						</li>
-						<li>
-							<a target="_blank" href="#">
-								<Image
-									width={0}
-									height={0}
-									src="/assets/images/instagram-filled.png"
-									alt="Instagram icon"
-									sizes="100vh"
-								/>
-							</a>
-						</li>
-						<li>
-							<a target="_blank" href="#">
-								<Image
-									width={0}
-									height={0}
-									src="/assets/images/youtube-filled.png"
-									alt="Youtube icon"
-									sizes="100vh"
-								/>
-							</a>
-						</li>
-					</ul>
+  return (
+    <>
+      <nav className={`${styles.nav} ${inter.className} ${isOpen ? styles.open : ""}`} ref={sidebarRef}>
+        <button
+          onClick={() => {
+            showSidebar(false);
+          }}
+          className={styles.close_button}
+        >
+          <div className={styles.close_button_wrapper}>
+            <div className={styles.close_line}></div>
+            <div className={styles.close_line}></div>
+          </div>
+        </button>
+        <ul className={styles.nav_links}>
+          <li>
+            <Link href={"/"}>О компании</Link>
+          </li>
+          <li>
+            <Link href={"/our_mission"}>Наша миссия</Link>
+          </li>
+          <li>
+            <Link href={"/news"}>Что нового</Link>
+          </li>
+        </ul>
+        <button className={styles.contact_us}>
+          <span>Связаться с нами</span> <ArrowTopRight />
+        </button>
+        <div className={styles.credits}>
+          <div className={styles.social_links}>
+            <Link className={styles.social_link} href="">
+              <FacebookIcon className={styles.icon + " " + styles.facebook} />
+            </Link>
+            <Link className={styles.social_link} href="">
+              <InstagramIcon className={styles.icon + " " + styles.instagram} />
+            </Link>
+            <Link className={styles.social_link} href="">
+              <YoutubeIcon className={styles.icon + " " + styles.youtube} />
+            </Link>
+          </div>
 
-					<div className={styles.copyright}>© Eurasia Group 2022</div>
-				</div>
-			</nav>
-			<div
-				className={`${styles.overlay} ${isOpen ? styles.visible : ""}`}></div>
-		</>
-	);
+          <div className={styles.copyright}>© Eurasia Group 2022</div>
+        </div>
+      </nav>
+      <div className={`${styles.overlay} ${isOpen ? styles.visible : ""}`}></div>
+    </>
+  );
 };
 
 export default Sidebar;
